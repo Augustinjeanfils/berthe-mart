6e48bc9325af1f4999a27d2749573638869b7948
Tonton-ios <elienernston33@gmail.com>
2025-11-25 10:15:27 -0500

    tiktok add

--- script.js
+++ script.js
@@ -578,8 +578,8 @@ function renderContactPage() {
                                     </svg>
                                 </a>
                                 <a href="https://www.tiktok.com/@boncoeurminimart?_r=1&_t=ZM-91hHFztPNpE" target="_blank" class="social-link tiktok">
-                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-                                        <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M16 3h-1.5a4.5 4.5 0 0 1-4.5 4.5A4.5 4.5 0 0 1 5.5 3 4.5 4.5 0 0 0 1 7.5 4.5 4.5 0 0 0 5.5 12 4.5 4.5 0 0 0 10 7.5V20a2 2 0 1 0 2 0V9h4V3z"></path>
                                     </svg>
                                 </a>
                             </div>
