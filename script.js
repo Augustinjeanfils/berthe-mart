97214064e0f8034689652b69c4f30a34e28f6062
Tonton-ios <elienernston33@gmail.com>
2025-11-27 10:10:22 -0500

    update color

--- script.js
+++ script.js
@@ -1781,7 +1781,7 @@ function renderDigicelDetails() {
     return `
         <div class="section section-gray" style="min-height: 100vh;">
             <div class="container" style="padding-top: 3rem; padding-bottom: 3rem; max-width: 600px;">
-                <h1 style="font-size: 2rem; margin-bottom: 2rem; text-align: center; color: #FF6600;">Paiement Digicel (Mon Cash)</h1>
+                <h1 style="font-size: 2rem; margin-bottom: 2rem; text-align: center; color: #ff0000ff;">Paiement Digicel (Mon Cash)</h1>
                 
                 <div class="payment-details" style="background: white; padding: 2rem; border-radius: 0.5rem; border: 2px solid #FF6600;">
                     <h3 style="color: var(--gray-900); margin-bottom: 1.5rem;">Instructions de paiement</h3>
